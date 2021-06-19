9d11e690fefb0bed760be54893e7e599256dff80
Horlamov <horlamov@MacBook-Pro-Horlamov.local>
2021-06-19 23:49:45 +0300

    add: settings loaders

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,29 @@ module.exports = {
         from: path.resolve(__dirname, 'src/favicon.ico'),
         to: path.resolve(__dirname, 'dist')
       }
-    ])
-	]
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css'
+    })
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+			},
+			{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader'
+        }
+      }
+		]
+	}
 	
 }
